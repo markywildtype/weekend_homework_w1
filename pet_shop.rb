a3915f2d6126e6f858fa35b4818158fb3fa9b267
Mark Blanford <markytorture@hotmail.com>
2017-11-26 15:14:49 +0000

    third optional test

--- pet_shop.rb
+++ pet_shop.rb
@@ -73,3 +73,9 @@ def customer_can_afford_pet(customer, new_pet)
     return false
   end
 end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  customer[:pets].push(pet)
+  pet_shop[:admin][:total_cash] += pet[:price]
+  pet_shop[:admin][:pets_sold] += 1
+end
